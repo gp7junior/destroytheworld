b5a097c62902502821ce65d851b74ac31f9a6b90
Gilberto P. da S. Junior <gp7junior@gmail.com>
2017-06-03 09:34:28 -0300

    hello world

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Gilberto Pedro da Silva Jr on 03/06/17.
+//  Copyright © 2017 Gilberto Pedro da Silva Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
